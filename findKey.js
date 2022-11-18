e5b18e2be8d654de5067c58f4e034f45c148ef90
lalababa <lalababadidi@gmail.com>
2022-11-17 21:25:53 -0500

    findKey problems but still with problems

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected, suppressLogging = false) {
+  expected = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2) // => "noma"
+
+  if (actual === expected) {
+    if (!suppressLogging) {
+      console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
+    }
+    return true;
+  } else {
+    if (!suppressLogging) {
+      console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
+    }
+    return false;
+  }
+};
+
+
+module.exports = assertEqual;
