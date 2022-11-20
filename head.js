9dba0b540564d9de3b692d4ebdd954d1c60dd1e9
lalababa <lalababadidi@gmail.com>
2022-11-19 22:01:02 -0500

    push head.js'

---
+++ head.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array.length > 0) {
+    return array[0];
+  } else {
+    return undefined;
+  }
+  
+}
+
+
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
