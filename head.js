4b195a1a2683a4c5cbeae4bc449fe5497c282416
lalababa <lalababadidi@gmail.com>
2022-11-21 15:25:54 -0500

    new changes here

--- head.js
+++ head.js
@@ -1,11 +1,14 @@
+const assertEqual = require('./assertEqual');
+
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+// const assertEqual = function(actual, expected) {
+//   // if (actual === expected) {
+//   //   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   // } else {
+//   //   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   // }
+// };
 
 const head = function(array) {
   if (array.length > 0) {
@@ -13,11 +16,4 @@ const head = function(array) {
   } else {
     return undefined;
   }
-  
 }
-
-
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+// s
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
